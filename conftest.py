737bf41333adbd0a04ba79ea3d8f49ace3d7c311
Andrew Usachev <usachev.com@gmail.com>
2023-10-19 19:23:39 +0300

    Added final course work

--- conftest.py
+++
@@ -1,8 +0,0 @@
-# conftest.py
-def pytest_addoption(parser):
-    parser.addoption(
-        "--language",
-        action="store",
-        default="ru",
-        help="Specify the language for the test run",
-    )
